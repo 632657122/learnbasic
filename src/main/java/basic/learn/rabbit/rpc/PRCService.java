7f50181f9e59d94d2adb140716df8edfabc8fb49
wangtao <632657122@qq.com>
2018-12-21 17:55:44 +0800

    rabbitMQ rpc

---
+++ src/main/java/basic/learn/rabbit/rpc/PRCService.java
@@ -0,0 +1,101 @@
+package basic.learn.rabbit.rpc;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author wangtao
+ * @date 2018-12-21
+ * @desc
+ */
+public class PRCService {
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    private static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+            final Channel channel = connection.createChannel();
+
+            //准备接收
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+            //一次只处理一个消息
+            channel.basicQos(1);
+
+            System.out.println(" [x] Awaiting RPC requests");
+
+            Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    AMQP.BasicProperties replyProps = new AMQP.BasicProperties()
+                            .builder()
+                            .correlationId(properties.getCorrelationId())
+                            .build();
+
+                    String response = "";
+
+                    try {
+                        String message = new String(body, "UTF-8");
+                        int n = Integer.parseInt(message);
+
+                        System.out.println(" [.] fib(" + message + ")");
+                        response += fib(n);
+                    } finally {
+                        //回调 队列名是接收时指定的队列名properties.getReplyTo(),参数replyProps里带上corrid
+                        String queueName = properties.getReplyTo();
+                        System.out.println("===返回的队列名:" + queueName);
+                        channel.basicPublish("", queueName, replyProps, response.getBytes("UTF-8"));
+                        long tag = envelope.getDeliveryTag();
+                        System.out.println("===tag:" + tag);
+                        channel.basicAck(envelope.getDeliveryTag(), false);//是否批量,true:一次性ack所有小于等于tag的消息,false:只ack index为tag的消息
+                        synchronized (this) {
+                            this.notify();
+                        }
+                    }
+                }
+            };
+
+            //消费rpc_queue里的任务
+            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+            while (true) {
+                synchronized (consumer) {
+                    try {
+                        consumer.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/basic/learn/rabbit/rpc/RPCClient.java
@@ -0,0 +1,94 @@
+package basic.learn.rabbit.rpc;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author wangtao
+ * @date 2018-12-21
+ * @desc
+ */
+public class RPCClient {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+
+    public RPCClient() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+
+    }
+
+    public String call(String message) throws Exception {
+        final String corrId = UUID.randomUUID().toString();//声明一个暗号,接收server消息时暗号要对上才能接收
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)//声明一个接收地,让server往这个队列返回消息
+                .build();
+
+        //向server发送请求,请求消息发送给了队列replyQueueName,另外在props里加上了接收地replyQueueName和暗号corrId
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+        //用BlockingQueue来延缓main线程,1代表我们只需要一个response
+        final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
+
+        //接收server返回的消息,对每一个收到的message都check一下corrid,相同的则put到BlockingQueue中
+        channel.basicConsume(replyQueueName, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws UnsupportedEncodingException {
+                if (properties.getCorrelationId().equals(corrId)) {
+                    response.offer(new String(body, "UTF-8"));
+
+                }
+            }
+        });
+        return response.take();
+
+    }
+
+    public void close() throws IOException {
+        connection.close();
+    }
+
+    public static void main(String[] args) {
+        RPCClient fibonacciRpc = null;
+        String response = null;
+        try {
+            fibonacciRpc = new RPCClient();
+            System.out.println(" [x] Requesting fib(30)");
+            response = fibonacciRpc.call("30");
+            System.out.println(" [.] Got '" + response + "'");
+
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fibonacciRpc != null) {
+                try {
+                    fibonacciRpc.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+}
