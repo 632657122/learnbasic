1a0adf9e9be9b7582f568bcb0b2c04bb75fb815d
wangtao <632657122@qq.com>
2018-12-25 11:53:06 +0800

    {}

--- src/main/java/basic/learn/bitset/BitSetDemo.java
+++ src/main/java/basic/learn/bitset/BitSetDemo.java
@@ -9,6 +9,25 @@ import java.util.BitSet;
  */
 public class BitSetDemo {
 
+    private static int cal;
+
+    public BitSetDemo() {
+        System.out.println("构造方法");
+    }
+
+    static {
+        if (String.valueOf(System.currentTimeMillis()).endsWith("1")) {
+            cal = 1;
+        } else {
+            cal = 3;
+        }
+        System.out.println("静态代码块");
+    }
+
+    {
+        System.out.println("普通代码块");
+    }
+
 
     public static void main(String[] args) {
         BitSet bitSet = new BitSet(64);
@@ -16,8 +35,9 @@ public class BitSetDemo {
         for (int i = 100; i >= 0; i--) {
             bitSet.set(i);
         }
-        bitSet.set(1,21,false);
+        bitSet.set(1, 21, false);
+        BitSetDemo b= new BitSetDemo();
 
-        System.out.println(bitSet.toString());
+        System.out.println(cal);
     }
 }
