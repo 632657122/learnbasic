da4fa48309dd97d7dae4276c504e9879692059da
wangtao <632657122@qq.com>
2019-01-08 09:54:36 +0800

    intern

--- src/main/java/basic/learn/intern/InternTest.java
+++ src/main/java/basic/learn/intern/InternTest.java
@@ -30,14 +30,16 @@ public class InternTest {
         所以"stringBuildertest"最先创建在堆中
          */
 
-        System.out.println(str2.intern() == "stringBuildertest");//true 此时会把"stringBuildertest"添加到常量区
+        System.out.println(str2.intern() == "stringBuildertest");//true  str2.intern()将"stringBuildertest"的引用添加到常量区
+        /*如果是 "stringBuildertest" ==str2.intern(),那么下面两条都为false，原因是会先将"stringBuildertest"添加到常量区 */
         System.out.println(str2.intern() == str2);//true
+        System.out.println(str2 == "stringBuildertest");//true 常量区中
         /*
         如果str2和str1字面量相同且str1先创建，则str2.intern()指向str1
          */
 
         String str3 = new StringBuilder("te").append("st").toString();
-        System.out.println(str3 == "test");//false  "test"字面量代表它在常量区的地址
+        System.out.println(str3 == "test");//false  "test"字面量代表它在常量区的地址或引用。若"test"或其引用不存在常量区，此时会把"test"添加到常量区
         System.out.println(str3.intern() == "test");//true
         System.out.println(str3.intern() == str3);//false 验证上面的结论,test在创建str2时已经被添加到常量区
 
