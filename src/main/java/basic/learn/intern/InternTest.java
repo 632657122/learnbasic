503ea1b8451aa7fc29aa1cf0b5c6d181462be69e
wangtao <632657122@qq.com>
2019-01-08 09:06:35 +0800

    intern

--- src/main/java/basic/learn/intern/InternTest.java
+++ src/main/java/basic/learn/intern/InternTest.java
@@ -8,9 +8,33 @@ package basic.learn.intern;
 public class InternTest {
 
     public static void main(String[] args) {
-        String str1 = new StringBuffer("stringBuilder").append("test").toString();
-        String str2 = new StringBuffer("stringBuilder").append("test").toString();
 
-        System.out.println(str2.intern() == str2);
+        //StringBuilder的toString()方法创建了一个new String()对象。
+        String str1 = new StringBuilder("stringBuilder").toString();
+        /*
+        上面代码等价于
+        String a = "stringBuilder";
+        String str1 = new StringBuilder(a).toString;
+        所以"stringBuilder"先被创建在常量池中
+        */
+        System.out.println(str1.intern() == "stringBuilder");//true
+        System.out.println(str1.intern() == str1);//false
+
+
+        String str2 = new StringBuilder("stringBuilder").append("test").toString();
+        /*
+        上面代码等价于
+        String a = "stringBuilder";
+        String b = "test";
+        String str2 = new StringBuilder(a).append(b).toString;
+        所以"stringBuildertest"最先创建在堆中
+         */
+
+        System.out.println(str2.intern() == "stringBuildertest");//true
+        System.out.println(str2.intern() == str2);//true
+        /*
+        如果str2和str1字面量相同且str1先创建，则str2.intern()指向str1
+         */
+
     }
 }
