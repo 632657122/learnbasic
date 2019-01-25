9a056579e66fdaed7ec83b18658a605e91489bcf
wangtao <632657122@qq.com>
2019-01-25 21:29:05 +0800

    netty

--- src/main/java/basic/learn/netty/demo1/server/HelloServer.java
+++ src/main/java/basic/learn/netty/demo1/server/HelloServer.java
@@ -2,15 +2,9 @@ package basic.learn.netty.demo1.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * @author wangtao
@@ -30,15 +24,7 @@ public class HelloServer {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup);
             b.channel(NioServerSocketChannel.class);
-            b.childHandler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()))
-                            .addLast("decoder", new StringDecoder())
-                            .addLast("encoder", new StringEncoder())
-                            .addLast("handler", new HelloServerHandler());
-                }
-            });
+            b.childHandler(new HelloServerInitializer());
 
             //服务器绑定端口监听
             ChannelFuture f = b.bind(port).sync();
---
+++ src/main/java/basic/learn/netty/discardserver/DiscardServerHandler.java
@@ -0,0 +1,27 @@
+package basic.learn.netty.discardserver;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author wangtao
+ * @date 2019-01-25
+ * @desc 处理服务端channel
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("收到消息为："+in);
+        ctx.writeAndFlush(in);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
